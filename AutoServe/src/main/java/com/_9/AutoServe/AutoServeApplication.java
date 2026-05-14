c4248ed5ccd549b2fe9846cc8c9b5d8adebb707d
IT25102176 <it25102176@my.sliit.lk>
2026-05-14 17:29:24 +0530

    Project setup

---
+++ AutoServe/src/main/java/com/_9/AutoServe/AutoServeApplication.java
@@ -0,0 +1,13 @@
+package com._9.AutoServe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServeApplication.class, args);
+	}
+
+}
---
+++ AutoServe/src/test/java/com/_9/AutoServe/AutoServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com._9.AutoServe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
