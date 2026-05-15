b749421f246ef2a3d92e4c7a98f6f39d4482f1da
IT25102230 <it25102230@my.sliit.lk>
2026-05-15 14:55:39 +0530

    Service Record Controller

---
+++ AutoServe/src/main/java/com/_9/AutoServe/Controller/ServiceRecordController.java
@@ -0,0 +1,42 @@
+package com._9.AutoServe.Controller;
+
+import com.se1020.carservice.model.ServiceRecord;
+import com.se1020.carservice.service.ServiceRecordService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/services")
+public class ServiceRecordController {
+    private final ServiceRecordService serviceRecordService;
+
+    public ServiceRecordController(ServiceRecordService serviceRecordService) {
+        this.serviceRecordService = serviceRecordService;
+    }
+
+    @GetMapping
+    public List<ServiceRecord> getAll() {
+        return serviceRecordService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ServiceRecord getById(@PathVariable String id) {
+        return serviceRecordService.getById(id);
+    }
+
+    @PostMapping
+    public ServiceRecord create(@RequestBody ServiceRecord serviceRecord) {
+        return serviceRecordService.create(serviceRecord);
+    }
+
+    @PutMapping("/{id}")
+    public ServiceRecord update(@PathVariable String id, @RequestBody ServiceRecord serviceRecord) {
+        return serviceRecordService.update(id, serviceRecord);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        serviceRecordService.delete(id);
+    }
+}
