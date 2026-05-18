79212eb6a0739a138e2e69108a34a21e48445dbc
IT25102988 <it25102988@my.sliit.lk>
2026-05-19 03:05:29 +0530

    Added comments

--- AutoServe/src/main/java/com/_9/AutoServe/repository/AppointmentRepository.java
+++ AutoServe/src/main/java/com/_9/AutoServe/repository/AppointmentRepository.java
@@ -5,10 +5,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import tools.jackson.databind.json.JsonMapper;
 
+/*
+     The Database Whisperer:
+     - writes the SQL queries bts to save, update, delete or fetch appointments from the database tables
+*/
+
 @Repository
-public class AppointmentRepository extends JsonFileRepository<Appointment> {
+public class AppointmentRepository extends JsonFileRepository<Appointment> {        // "Inheritance"
     public AppointmentRepository(JsonMapper objectMapper, @Value("${app.storage.dir:data}") String storageDir) {
         super(objectMapper, storageDir, "appointments.json", Appointment.class, Appointment::getId);
     }
 }
-
+// Constructor: runs at startup, spring Boot automatically hands it the JsonMapper translator.
